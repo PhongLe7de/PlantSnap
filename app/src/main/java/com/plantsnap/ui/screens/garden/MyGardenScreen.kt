2193aec64fecbc3287677090ebff2004518fabd9
Timppav <tino.vuorela@gmail.com>
2026-05-05 19:49:18 +0300

    Fix redundant padding

--- app/src/main/java/com/plantsnap/ui/screens/garden/MyGardenScreen.kt
+++ app/src/main/java/com/plantsnap/ui/screens/garden/MyGardenScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -111,6 +112,7 @@ private fun MyGardenScreenContent(
     Scaffold(
         modifier = Modifier.testTag("screen_garden"),
         containerColor = scheme.surface,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = { MyGardenTopBar(onAddClick = onAddSpecimen) },
     ) { innerPadding ->
         LazyColumn(
--- app/src/main/java/com/plantsnap/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/plantsnap/ui/screens/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -106,6 +107,7 @@ fun HomeScreenContent(
     Scaffold(
         modifier = Modifier.testTag("screen_home"),
         containerColor = scheme.surface,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = {
             TopBar(
                 profilePhotoUrl = profilePhotoUrl,
